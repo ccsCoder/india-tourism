cfbc7cae10fac96e1ddefaf4ea68b89097b2be5b
Nishant Gupta <ccsCoder@gmail.com>
2020-08-23 13:41:30 +0530

    Correcting text position in map

---
+++ js/index.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // const mapContainer = document.querySelector('.map');
+  // const svg = mapContainer.querySelector('svg');
+  // const mapDimensions = mapContainer.getBoundingClientRect();
+
+  // const viewBox = `${mapDimensions.left} ${mapDimensions.top} ${mapDimensions.width} ${mapDimensions.height}`
+  // svg.setAttribute('viewBox', viewBox);
+  // svg.setAttribute('preserveAspectRatio','xMidYMid meet');
+});
